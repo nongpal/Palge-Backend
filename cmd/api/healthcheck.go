2161727f40b0132e6ca0962d77be58651ec398bd
Nongpal <naufalhafish666@gmail.com>
2026-07-14 12:06:38 +0700

    feat: add healthcheck endpoint
    Refs #4

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -2,4 +2,16 @@ package main
 
 import "net/http"
 
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {}
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.cfg.env,
+			"version":     version,
+		},
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, data, nil); err != nil {
+		app.logger.Error(err.Error())
+	}
+}
