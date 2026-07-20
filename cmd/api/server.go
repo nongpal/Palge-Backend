f3a9f960c744089bf7704cf070dc82b8e4977a5d
Nongpal <naufalhafish666@gmail.com>
2026-07-20 15:37:21 +0700

    refactor: remove unnecessry info

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-<<<<<<< HEAD
-=======
 	"context"
 	"errors"
->>>>>>> aa3203b (feat: Implement graceful shutdown)
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -26,29 +23,21 @@ func (app *application) serve() error {
 		ErrorLog:          slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
-<<<<<<< HEAD
-=======
 	shutdownErr := make(chan error)
 
->>>>>>> aa3203b (feat: Implement graceful shutdown)
 	go func() {
 		quit := make(chan os.Signal, 1)
 
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
 		s := <-quit
-<<<<<<< HEAD
-		app.logger.Info("caught signal", "signal", s.String())
 
-		os.Exit(0)
-=======
 		app.logger.Info("shutting down server", "signal", s.String())
 
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 
 		shutdownErr <- srv.Shutdown(ctx)
->>>>>>> aa3203b (feat: Implement graceful shutdown)
 	}()
 
 	app.logger.Info(
@@ -57,9 +46,6 @@ func (app *application) serve() error {
 		"env", app.cfg.env,
 	)
 
-<<<<<<< HEAD
-	return srv.ListenAndServe()
-=======
 	err := srv.ListenAndServe()
 	if !errors.Is(err, http.ErrServerClosed) {
 		return err
@@ -72,5 +58,4 @@ func (app *application) serve() error {
 
 	app.logger.Info("stopped server", "addr", srv.Addr)
 	return nil
->>>>>>> aa3203b (feat: Implement graceful shutdown)
 }
