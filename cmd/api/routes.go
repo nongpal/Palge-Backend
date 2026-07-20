c4f8b26a65b3d44909adde79e27f08af9d44b435
Nongpal <naufalhafish666@gmail.com>
2026-07-20 14:20:33 +0700

    feat: add slowHandler for check graceful shutdown
    Refs #6

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,6 +6,7 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("GET /v1/slow", app.slowHandler)
 
 	return mux
 }
