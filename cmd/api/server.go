96537af4938df6039bfe85f30f9b61f4e76b5f4f
Nongpal <naufalhafish666@gmail.com>
2026-07-20 14:18:17 +0700

    feat: Implement graceful shutdown
    Refs #6
    - calling `srv.Shutdown()`
    - using shutdown timeout
    - log shutdown process
    - return any shutdown errors

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+<<<<<<< HEAD
+=======
+	"context"
+	"errors"
+>>>>>>> aa3203b (feat: Implement graceful shutdown)
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -21,15 +26,29 @@ func (app *application) serve() error {
 		ErrorLog:          slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
 	}
 
+<<<<<<< HEAD
+=======
+	shutdownErr := make(chan error)
+
+>>>>>>> aa3203b (feat: Implement graceful shutdown)
 	go func() {
 		quit := make(chan os.Signal, 1)
 
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
 		s := <-quit
+<<<<<<< HEAD
 		app.logger.Info("caught signal", "signal", s.String())
 
 		os.Exit(0)
+=======
+		app.logger.Info("shutting down server", "signal", s.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdownErr <- srv.Shutdown(ctx)
+>>>>>>> aa3203b (feat: Implement graceful shutdown)
 	}()
 
 	app.logger.Info(
@@ -38,5 +57,20 @@ func (app *application) serve() error {
 		"env", app.cfg.env,
 	)
 
+<<<<<<< HEAD
 	return srv.ListenAndServe()
+=======
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownErr
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info("stopped server", "addr", srv.Addr)
+	return nil
+>>>>>>> aa3203b (feat: Implement graceful shutdown)
 }
