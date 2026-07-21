22d514cf1cd854a3ba519ecc555f12a2e6178e16
Nongpal <naufalhafish666@gmail.com>
2026-07-21 15:30:08 +0700

    feat: add readIDParam for parsing ID

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -110,3 +110,14 @@ func (app *application) readInt(qs url.Values, key string, defaultValue int) int
 
 	return i
 }
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := r.PathValue("id")
+
+	id, err := strconv.ParseInt(params, 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
