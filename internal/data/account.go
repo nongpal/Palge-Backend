ea9d6b9e2c14b449c78fbe9f80467030b997be7a
nongpal <naufalhafish666@gmail.com>
2026-07-24 23:37:26 +0700

    chore: make amount validation skeleton

--- internal/data/account.go
+++ internal/data/account.go
@@ -1,7 +1,13 @@
 package data
 
+import "github.com/nongpal/Palge-Backend/internal/validator"
+
 type Account struct {
 	ID      int64  `json:"id"`
 	Owner   string `json:"owner"`
 	Balance int64  `json:"balance"`
+}
+
+func ValidateAmount(v *validator.Validator, amount int64) {
+
 }
