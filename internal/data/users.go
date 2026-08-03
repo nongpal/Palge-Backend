5fc82fbf5349a2d98ac090fb43e40b1c70ed5dfc
Nongpal <naufalhafish666@gmail.com>
2026-08-03 14:55:58 +0700

    feat: create User type for a single user
    Refs #42

---
+++ internal/data/users.go
@@ -0,0 +1,18 @@
+package data
+
+import "time"
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
