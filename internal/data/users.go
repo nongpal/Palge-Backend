8c7d85ae76ccba69cf0ea88947c00fea5770c347
Nongpal <naufalhafish666@gmail.com>
2026-08-03 14:58:53 +0700

    feat: implement Set method
    use encrypt the password from user then store it in `password` struct
    Refs #42

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type User struct {
 	ID        int64     `json:"id"`
@@ -15,4 +19,15 @@ type User struct {
 type password struct {
 	plaintext *string
 	hash      []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return nil
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+	return nil
 }
