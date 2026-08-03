ce6fdcf1d744c4824eeb1c05ba6a38296dd958b0
Nongpal <naufalhafish666@gmail.com>
2026-08-03 15:14:27 +0700

    feat: implement validator check
    Refs #42

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -5,6 +5,10 @@ import (
 	"slices"
 )
 
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
 type Validator struct {
 	Errors map[string]string
 }
