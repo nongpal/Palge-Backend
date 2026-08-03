faced83d86723fa9fcc68ad9d404733fa9143269
Nongpal <naufalhafish666@gmail.com>
2026-08-03 15:41:14 +0700

    chore: Implement User Model
    - download bcrypt package
    - add error object
    
    Refs #42

--- internal/data/models.go
+++ internal/data/models.go
@@ -1,13 +1,23 @@
 package data
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
 
 type Models struct {
 	Accounts AccountModel
+	Users    UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Accounts: AccountModel{DB: db},
+		Users:    UserModel{DB: db},
 	}
 }
