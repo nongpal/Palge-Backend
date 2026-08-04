e908cf5a0f126f85d1681eef701e2ad590fa0134
Nongpal <naufalhafish666@gmail.com>
2026-08-04 16:39:00 +0700

    feat: add configuration and application for mailer
    Refs #45

--- internal/api/users.go
+++ internal/api/users.go
@@ -52,6 +52,12 @@ func (app *Application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	err = app.mailer.Send(users.Email, "user_welcome.tmpl.html", users)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	if err := app.writeJSON(w, http.StatusCreated, envelope{
 		"user": users,
 	}, nil); err != nil {
