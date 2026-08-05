f34641174fd842b842c86f1c9028762fcc96940b
nongpal <naufalhafish666@gmail.com>
2026-08-05 20:09:50 +0700

    feat: add increment and decrement for WaitGroup
    Refs #46

--- internal/api/helpers.go
+++ internal/api/helpers.go
@@ -123,7 +123,9 @@ func (app *Application) readIDParam(r *http.Request) (int64, error) {
 }
 
 func (app *Application) background(fn func()) {
+	app.wg.Add(1)
 	go func() {
+		defer app.wg.Done()
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.Error(fmt.Sprintf("%v", err))
