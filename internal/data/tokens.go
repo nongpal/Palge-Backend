35438dd5d44bf625bb3bf8e32d33a815cb27fdf4
nongpal <naufalhafish666@gmail.com>
2026-08-07 14:54:02 +0700

    feat: implement delete all users
    Refs #48

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -67,3 +67,16 @@ func (m *TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token,
 	err := m.Insert(token)
 	return token, err
 }
+
+func (m *TokenModel) DeleteAllForUser(scope string, userID int64) error {
+	query := `
+	DELETE FROM tokens
+	WHERE scope = $1 AND user_id = $2
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, query, scope, userID)
+	return err
+}
