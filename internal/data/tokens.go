f6d3e2bab3e71b151b2cdbf89a118b1b1a7b41c8
nongpal <naufalhafish666@gmail.com>
2026-08-07 14:23:03 +0700

    feat: implement activation token
    Refs #48

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"time"
+)
 
 type Token struct {
 	Plaintext string
@@ -8,4 +12,18 @@ type Token struct {
 	UserID    int64
 	Expiry    time.Time
 	Scope     string
+}
+
+func generateToken(userID int64, ttl time.Duration, scope string) *Token {
+	token := &Token{
+		Plaintext: rand.Text(),
+		UserID:    userID,
+		Expiry:    time.Now().Add(ttl),
+		Scope:     scope,
+	}
+
+	hash := sha256.Sum256([]byte(token.Plaintext))
+	token.Hash = hash[:]
+
+	return token
 }
