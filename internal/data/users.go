7cc662c1c7631a9f2366c7cdbb0c465c0679ea56
nongpal <naufalhafish666@gmail.com>
2026-08-11 10:43:11 +0700

    feat: add anonymous user struct

--- internal/data/users.go
+++ internal/data/users.go
@@ -16,6 +16,8 @@ var (
 	ErrDuplicateEmail = errors.New("duplicate email")
 )
 
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -33,6 +35,10 @@ type UserModel struct {
 type password struct {
 	plaintext *string
 	hash      []byte
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 func (p *password) Set(plaintextPassword string) error {
