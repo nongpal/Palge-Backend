b284e65cbe8aeb1ddff3b5c982b5abc5e0e0b36a
nongpal <naufalhafish666@gmail.com>
2026-08-11 10:42:24 +0700

    feat: create user reading context helper for auth middleware
    Refs #49

---
+++ internal/api/context.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/nongpal/Palge-Backend/internal/data"
+)
+
+type contextKey string
+
+const userContextKey = contextKey("user")
+
+func (app *Application) contextSetUser(r *http.Request, user *data.User) *http.Request {
+	ctx := context.WithValue(r.Context(), userContextKey, user)
+	return r.WithContext(ctx)
+}
+
+func (app *Application) contextGetUser(r *http.Request) *data.User {
+	user, ok := r.Context().Value(userContextKey).(*data.User)
+	if !ok {
+		panic("missing user value in request context")
+	}
+	return user
+}
