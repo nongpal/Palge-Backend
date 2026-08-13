53e2fac13df86bb1a3dac706e2c7f22dadd146fb
nongpal <naufalhafish666@gmail.com>
2026-08-13 16:36:58 +0700

    feat: automatically add read permission for new users
    Refs #50

--- internal/api/users.go
+++ internal/api/users.go
@@ -52,6 +52,12 @@ func (app *Application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	err = app.models.Permissions.AddForUser(users.ID, "accounts:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	token, err := app.models.Tokens.New(users.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
