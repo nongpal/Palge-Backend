9988bc5a52ec6450962658dca3dcae94a9dd7cc1
nongpal <naufalhafish666@gmail.com>
2026-09-08 19:39:09 +0700

    feat(api): record registration audit logging
    Refs #52

--- internal/api/users.go
+++ internal/api/users.go
@@ -24,24 +24,31 @@ func (app *Application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 
 	v := validator.New()
 
-	users := &data.User{
+	user := &data.User{
 		Name:      input.Name,
 		Email:     input.Email,
 		Activated: false,
 	}
 
-	if err := users.Password.Set(input.Password); err != nil {
+	if err := user.Password.Set(input.Password); err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
 
-	data.ValidateUser(v, users)
+	data.ValidateUser(v, user)
 	if !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
-	if err := app.models.Users.Insert(users); err != nil {
+	if err := app.models.Users.Insert(user); err != nil {
+		app.recordAuditLog(r, &data.AuditLog{
+			UserID:     nil,
+			Action:     "register",
+			Resource:   "user",
+			ResourceID: nil,
+			Result:     "failed",
+		})
 		switch {
 		case errors.Is(err, data.ErrDuplicateEmail):
 			v.AddError("email", "a user with this email address already exists")
@@ -52,7 +59,7 @@ func (app *Application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	token, err := app.models.Tokens.New(users.ID, 3*24*time.Hour, data.ScopeActivation)
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
@@ -61,16 +68,24 @@ func (app *Application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	app.background(func() {
 		data := map[string]any{
 			"activationToken": token.Plaintext,
-			"userID":          users.ID,
+			"userID":          user.ID,
 		}
-		err = app.mailer.Send(users.Email, "user_welcome.tmpl.html", data)
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", data)
 		if err != nil {
 			app.logger.Error(err.Error())
 		}
 	})
 
+	app.recordAuditLog(r, &data.AuditLog{
+		UserID:     nil,
+		Action:     "register",
+		Resource:   "user",
+		ResourceID: &user.ID,
+		Result:     "success",
+	})
+
 	if err := app.writeJSON(w, http.StatusAccepted, envelope{
-		"user": users,
+		"user": user,
 	}, nil); err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
