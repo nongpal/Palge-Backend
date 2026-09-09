2dbbcc604ae68cb1421a1b62ccbffa68d4ac7962
nongpal <naufalhafish666@gmail.com>
2026-09-10 00:06:15 +0700

    feat(api): implement method for custom response
    Refs #53

--- internal/api/response.go
+++ internal/api/response.go
@@ -7,4 +7,15 @@ type responseWriter struct {
 	status int
 }
 
+func (rw *responseWriter) WriteHeader(statusCode int)  {
+	rw.status = statusCode
+	rw.ResponseWriter.WriteHeader(statusCode)
+}
 
+func (rw *responseWriter) Write(b []byte) (int, error ) {
+	if rw.status == 0 {
+		rw.status = http.StatusOK
+	}
+
+	return rw.ResponseWriter.Write(b)
+}
