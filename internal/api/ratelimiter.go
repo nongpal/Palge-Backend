eb39601d132458d0a9065ce16ed00f7f903d144c
nongpal <naufalhafish666@gmail.com>
2026-09-13 13:53:23 +0700

    feat(api): implement in-memory rate limiter
    Refs #54

---
+++ internal/api/ratelimiter.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"net/http"
+
+	"golang.org/x/time/rate"
+)
+
+func (app *Application) rateLimit(next http.Handler) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
